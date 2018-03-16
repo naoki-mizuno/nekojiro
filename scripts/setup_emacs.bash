c285a39fdbe342816eb49a1b611657d803d81e5c
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-03-17 01:20:40 +0900

    Fix directory name

--- scripts/setup_emacs.bash
+++ scripts/setup_emacs.bash
@@ -6,5 +6,5 @@ if ! _is_installed emacs24-nox; then
     _install emacs24-nox
 fi
 
-mkdir -p ~/.emacs
-ln -s $PWD/resources/init.el ~/.emacs/init.el
+mkdir -p ~/.emacs.d
+ln -s $PWD/resources/init.el ~/.emacs.d/init.el
