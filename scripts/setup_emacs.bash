2b1ec7417c1bca57718b7098447a82c35932028e
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-03-17 01:23:17 +0900

    Rename current configuration

--- scripts/setup_emacs.bash
+++ scripts/setup_emacs.bash
@@ -6,5 +6,9 @@ if ! _is_installed emacs24-nox; then
     _install emacs24-nox
 fi
 
+if [[ -f ~/.emacs ]]; then
+    mv ~/.emacs ~/.emacs_old
+fi
+
 mkdir -p ~/.emacs.d
 ln -s $PWD/resources/init.el ~/.emacs.d/init.el
